a0b3ff4664224bf3c91a3bf5a0c38eb5c5aafcec
家牛 <jianiu.lj@antfin.com>
2021-09-14 22:51:16 +0800

    更新文件名

--- app/model/src/test/java/com/alipay/sofa/doc/model/MenuItemTest.java
+++ app/model/src/test/java/com/alipay/sofa/doc/model/MenuItemTest.java
@@ -13,11 +13,13 @@ public class MenuItemTest {
         Assert.assertEquals("xxx", service.url2Slug("xxx"));
         Assert.assertEquals("xxx", service.url2Slug("XXX"));
         Assert.assertEquals("xxx", service.url2Slug("XxX.md"));
-        Assert.assertEquals("b", service.url2Slug("/aa/b"));
-        Assert.assertEquals("b", service.url2Slug("/aa/b.md"));
-        Assert.assertEquals("b", service.url2Slug("aa/b.md"));
-        Assert.assertEquals("b", service.url2Slug("/aa/b.MD"));
-        Assert.assertEquals("c", service.url2Slug("/aa/bb/c.md"));
-        Assert.assertEquals("c", service.url2Slug("/aa/b-b/c.md"));
+        Assert.assertEquals("aa-b", service.url2Slug("/aa/b"));
+        Assert.assertEquals("aa-b", service.url2Slug("/aa/b.md"));
+        Assert.assertEquals("aa-b", service.url2Slug("aa/b.md"));
+        Assert.assertEquals("aa-b", service.url2Slug("/aa/b.MD"));
+        Assert.assertEquals("aa-bb-c", service.url2Slug("/aa/bb/c.md"));
+        Assert.assertEquals("aa-b-b-c", service.url2Slug("/aa/b-b/c.md"));
+        Assert.assertEquals("aa-b-b-c", service.url2Slug("/aa/b-b/c中文.md"));
+        Assert.assertEquals("event_extraction-readme", service.url2Slug("solutions/knowledge_mining/event_extraction/README.md"));
     }
 }
