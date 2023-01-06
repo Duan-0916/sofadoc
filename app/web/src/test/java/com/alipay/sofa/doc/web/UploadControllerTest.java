e0a866a849e3c3696a400c7a378b79ace9def9ea
Geng Zhang <zhanggeng.zg@antfin.com>
2023-01-06 12:10:40 +0800

    fix log.

--- app/web/src/test/java/com/alipay/sofa/doc/web/UploadControllerTest.java
+++ app/web/src/test/java/com/alipay/sofa/doc/web/UploadControllerTest.java
@@ -15,4 +15,29 @@ public class UploadControllerTest {
 -F "yuqueUser=zhanggeng.zg" \
 http://127.0.0.1:8888/v1/rest/syncByZip
      */
+
+        /*
+    curl -i -X POST -H 'Content-Type: multipart/form-data' \
+-F "file=@test-doc.zip" \
+-F "yuqueNamespace=zhanggeng.zg/whyya9" \
+-F "gitRepo=http://gitlab.alipay-inc.com/zhanggeng.zg/test-doc.git" \
+-F "yuqueUser=zhanggeng.zg" \
+http://127.0.0.1:8888/v1/rest/syncByZip
+
+
+    这种我需要提前配置 yuqueUser 的 token 到我这边的平台。
+     */
+
+     /*
+    curl -i -X POST -H 'Content-Type: multipart/form-data' \
+-F "file=@test-doc.zip" \
+-F "yuqueNamespace=zhanggeng.zg/whyya9" \
+-F "gitRepo=http://gitlab.alipay-inc.com/zhanggeng.zg/test-doc.git" \
+-F "yuqueToken=xxxxxxxxxxxxxxx" \
+http://sofadoc.alipay.com/v1/rest/syncByZip
+
+    file 就是你的文档git库的压缩包
+    xxxxxxxxxxxxxxx 换成你的语雀团队token
+     */
+
 }
